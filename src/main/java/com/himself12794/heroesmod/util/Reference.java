8f92ff5eabeb1ab3913a04f317e591ba13929359
Himself12794 <pwhiting.bba@gmail.com>
2015-09-15 14:06:37 -0400

    updates to mcmod.info

--- src/main/java/com/himself12794/heroesmod/util/Reference.java
+++ src/main/java/com/himself12794/heroesmod/util/Reference.java
@@ -4,23 +4,22 @@ package com.himself12794.heroesmod.util;
 /**
  * Strings constants used throughout the mod.
  * 
- * @author phwhitin
+ * @author Himself12794
  *
  */
 public abstract class Reference {
 	
     public static final String MODID = "HeroesMod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.8-rev11";
     public static final String NAME = "Heroes Mod";
     public static final String AUTHOR = "Himself12794";
-    public static final String DEPENDENCIES = "required-after:powersAPI";
     public static final String CLIENT_PROXY = "com.himself12794.heroesmod.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.himself12794.heroesmod.proxy.CommonProxy";
     
     /**
      * References to sounds.
      * 
-     * @author Philip
+     * @author Himself12794
      *
      */
     public static abstract class Sounds {
